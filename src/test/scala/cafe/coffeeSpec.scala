824b5fbddb6d4b761ae41cfff9605a05776d8444
Andrew Armstrong <andrew.armstrong@digital.hmrc.gov.uk>
2018-03-14 11:38:28 +0000

    initial test created

--- src/test/scala/cafe/coffeeSpec.scala
+++ src/test/scala/cafe/coffeeSpec.scala
@@ -1,5 +1,14 @@
 package cafe
 
-class coffeeSpec {
+import org.scalatest.{MustMatchers, WordSpec}
 
+
+class coffeeSpec extends WordSpec with MustMatchers {
+
+  "Cafe" in {
+
+    "return heatedWater temp as '40' when given water" in {
+      heat(Water(20), 40) mustEqual Water(40)
+    }
+  }
 }
