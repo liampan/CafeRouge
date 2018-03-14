be122df5edbb196a70ed03364854eee8a9e96685
Liam Oldfield <liam.oldfield@digital.hmrc.gov.uk>
2018-03-14 14:51:02 +0000

    added exceptions

--- src/test/scala/cafe/coffeeSpec.scala
+++ src/test/scala/cafe/coffeeSpec.scala
@@ -12,24 +12,49 @@ class coffeeSpec extends WordSpec with MustMatchers {
       coffee.heat(Water(20), 40) mustEqual Water(40)
     }
 
-    "return heatedWater temp as '40' when no temperature is given" ignore {
-
-      coffee.heat(Water(20), 0) mustEqual Water(40)
-    }
-
     "return 'GroundCoffee' when 'CoffeeBeans' given to grinder" in {
 
       coffee.grind(coffeeBean("ArabicaBeans")) mustEqual groundCoffee("ArabicaBeans")
     }
+    "return 'not real beans' when given 'jelly beans' " in {
+
+      val i = intercept[BeanException](coffee.grind(coffeeBean("jellyBeans")))
+
+       i.getMessage mustEqual "jellyBeans are not accepted"
+    }
+
 
     "return 'FrothedMilk' when given 'WholeMilk'" in {
 
       coffee.frother(milk("WholeMilk")) mustEqual frothedMilk("WholeMilk")
     }
 
-    "return 'Espresso' when given 'Water', 'GroundCoffee' and 'FrothedMilk'" in {
+    "return 'FrothedMilk' when given 'SkimmedMilk'" in {
+
+      coffee.frother(milk("SkimmedMilk")) mustEqual frothedMilk("SkimmedMilk")
+    }
+
+    "return illegalArg... when given 'SemiSkimmedMilk'" in {
+
+      val i = intercept[IllegalArgumentException](coffee.frother(milk("SemiSkimmedMilk")))
+
+      i.getMessage mustEqual "Your milk is minging fam"
+
+
+    }
+
+
+    "return 'Espresso' when given 'Water', 'GroundCoffee' " in {
 
       coffee.brew(groundCoffee("ArabicaBeans"), Water(50)) mustEqual espresso(50)
+    }
+
+    "return 'Too cold' when given 'Water of 35' 'GroundCoffee'" in {
+
+      val i = intercept[BrewException](coffee.brew(groundCoffee("ArabicaBeans"), Water(35)))
+
+      i.getMessage mustEqual "The water is too cold"
+
     }
 
     "return 'Latte' when given 'espresso' and 'frothedMilk" in {
@@ -40,6 +65,7 @@ class coffeeSpec extends WordSpec with MustMatchers {
 
       coffee.barista(coffeeBean("ArabicaBeans"), milk("WholeMilk"), 50) mustEqual latte("WholeMilk", 45)
     }
+
 
 
 
