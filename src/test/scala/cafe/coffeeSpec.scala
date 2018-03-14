2434e06b8b78c9ee4d5c383227f24e1a06e5df65
Andrew Armstrong <andrew.armstrong@digital.hmrc.gov.uk>
2018-03-14 13:55:50 +0000

    combine ingredients test passed

--- src/test/scala/cafe/coffeeSpec.scala
+++ src/test/scala/cafe/coffeeSpec.scala
@@ -22,6 +22,25 @@ class coffeeSpec extends WordSpec with MustMatchers {
       coffee.grind(coffeeBean("ArabicaBeans")) mustEqual groundCoffee("ArabicaBeans")
     }
 
+    "return 'FrothedMilk' when given 'WholeMilk'" in {
+
+      coffee.frother(milk("WholeMilk")) mustEqual frothedMilk("WholeMilk")
+    }
+
+    "return 'Espresso' when given 'Water', 'GroundCoffee' and 'FrothedMilk'" in {
+
+      coffee.brew(groundCoffee("ArabicaBeans"), Water(50)) mustEqual espresso(50)
+    }
+
+    "return 'Latte' when given 'espresso' and 'frothedMilk" in {
+      coffee.combine(espresso(50), frothedMilk("WholeMilk")) mustEqual latte("WholeMilk", 45)
+    }
+
+    "return 'Latte(WholeMilk, 50)' when given 'ArabicaBeans, WholeMilk, 50'" in {
+
+      coffee.barista(coffeeBean("ArabicaBeans"), milk("WholeMilk"), 50) mustEqual latte("WholeMilk", 45)
+    }
+
 
 
 
