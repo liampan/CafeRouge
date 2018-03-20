8bf57467ccc834bc4d0dcc1d4d52a8abe1ab126e
Andrew Armstrong <andrew.armstrong@digital.hmrc.gov.uk>
2018-03-20 09:09:51 +0000

    futures added, some tests refactored and working

--- src/main/scala/cafe/Coffee.scala
+++ src/main/scala/cafe/Coffee.scala
@@ -1,4 +1,7 @@
 package cafe
+import java.util.concurrent.Executors
+
+import scala.concurrent.{ExecutionContext, Future}
 import scala.util.Random
 
 case class Water(temp : Int)
@@ -8,6 +11,7 @@ case class Milk(milkType : String)
 case class FrothedMilk(fat : String)
 case class Espresso(beanType : GroundCoffee, temp : Int)
 case class Latte(fatFactor : String, temp : Int)
+case class CoffeeCup(shot : Espresso, frothedMilk : FrothedMilk)
 
 case class BeanException(msg : String) extends Exception(msg)
 case class BrewException(msg : String) extends Exception(msg)
@@ -15,12 +19,15 @@ case class BrewException(msg : String) extends Exception(msg)
 
 object Coffee extends App {
 
-  def heat(water : Water, heatTo : Int) : Water = {
+  implicit def ec : ExecutionContext = ExecutionContext.fromExecutor(Executors.newCachedThreadPool())
+
+
+  def heat(water : Water, heatTo : Int) : Future[Water] = Future {
     Thread.sleep(Random.nextInt(3000))
     water.copy(heatTo)
   }
 
-  def grind(beanInput : CoffeeBean) : GroundCoffee = {
+  def grind(beanInput : CoffeeBean) : Future[GroundCoffee] = Future {
     lazy val grinder = {
       println(s"currently grinding ${beanInput.beanType}")
       Thread.sleep(Random.nextInt(1000))
@@ -33,7 +40,7 @@ object Coffee extends App {
 
   }
 
-  def milkFoam(milkInput : Milk) : FrothedMilk = {
+  def milkFoam(milkInput : Milk) : Future[FrothedMilk] = Future {
     lazy val foam = {println(s"frothing ${milkInput.milkType}")
       Thread.sleep(Random.nextInt(1000))
       FrothedMilk(milkInput.milkType)}
@@ -46,25 +53,37 @@ object Coffee extends App {
 
   }
 
-  def brew(coffee : GroundCoffee, water : Water) : Espresso = {
+  def brew(coffee : GroundCoffee, water : Water) : Future[Espresso] = Future {
   if (water.temp <40) {throw BrewException("The water is too cold")}
   else {Thread.sleep(Random.nextInt(1000))
     Espresso(coffee, water.temp)}
   }
 
-  def combine(espresso : Espresso, milk : FrothedMilk) : Latte = {
+  def combine(espresso : Espresso, milk : FrothedMilk) : Future[Latte] = Future {
     Thread.sleep(Random.nextInt(1000))
     Latte(milk.fat, espresso.temp-5)
   }
 
-  def barista(bean: CoffeeBean, milk: Milk, temp: Int) : Latte = {
+  def barista(bean: CoffeeBean, milk: Milk, temp: Int) : Future[CoffeeCup] = {
+
+
     val hotWater = heat(Water(20), temp)
     val ground = grind(bean)
     val foam = milkFoam(milk)
-    val shot = brew(ground,hotWater)
+
     println(s"You have brewed the following coffee: Coffee made with ${bean.beanType} at $temp degrees with ${milk.milkType}")
 
-    combine(shot, foam)
+    for {
+      water <- hotWater
+      frothedMilk <- foam
+      groundCoffee <- ground
+      shot <- brew(groundCoffee,water)
+    } yield {
+      CoffeeCup(shot, frothedMilk)
+    }
+
+//Need to look at implicits code
+
   }
 
 }
